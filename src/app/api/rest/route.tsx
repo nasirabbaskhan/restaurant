eb6bbe3baf132a71cb951a3ae4eb3fd7a2451c38
nasirabbaskhan <akhtarabbas.islamicreaction@gmail.com>
2024-04-05 11:47:45 -0700

    mongo db conected

---
+++ src/app/api/rest/route.tsx
@@ -0,0 +1,21 @@
+import { NextRequest, NextResponse } from "next/server";
+import mongoose from "mongoose";
+
+import dbConnect from "@/lib/db";
+import { userSchema } from "@/model/usersModel";
+
+export async function GET() {
+  try {
+    await dbConnect(); // Connect to the database
+    console.log("Database is successfully connected");
+
+    // Find documents using the userSchema
+    const data = await userSchema.find();
+    console.log("nasir aneela:", data);
+    return NextResponse.json(data);
+    // Return JSON response
+  } catch (error) {
+    console.log("db is not conneted");
+    console.log("SOMTHING WRONG", error);
+  }
+}
