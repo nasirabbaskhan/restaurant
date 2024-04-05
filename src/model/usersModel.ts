b1703c973d61ae9d9f36d9ec687c3ce7289bb6a7
nasirabbaskhan <akhtarabbas.islamicreaction@gmail.com>
2024-04-05 13:01:29 -0700

    done

--- src/model/usersModel.ts
+++
@@ -1,8 +0,0 @@
-// import mongoose from "mongoose";
-
-// const userModel = new mongoose.Schema({
-//   name: String,
-// });
-
-// export const userSchema =
-//   mongoose.models.users || mongoose.model("users", userModel);
