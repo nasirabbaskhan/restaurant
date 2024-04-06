ca6c204a5e58da7ef3de2fc63cee96595d51c2e2
nasirabbaskhan <akhtarabbas.islamicreaction@gmail.com>
2024-04-05 22:21:53 -0700

    done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,36 +1,30 @@
-// Define the type for the fetchData function
-// export type FetchDataFunction = () => Promise<DbData[]>;
 // interface DbData {
 //   _id: string;
 //   name: string;
 // }
-// export type FetchDataFunction = () => Promise<DbData[]>;
 
 // Your fetchData function implementation
-// const fetchData = async () => {
-//   const res = await fetch("http://localhost:3000/api/rest", {
-//     cache: "no-store",
-//   });
-//   const response = await res.json();
-//   console.log("res new", response);
-//   return response;
-// };
+const fetchData = async () => {
+  const res = await fetch("http://localhost:3000/api/rest", {
+    cache: "no-store",
+  });
+  const response = await res.json();
+  console.log("res new", response);
+  return response;
+};
 export default async function Home() {
-  // const res = await fetch("http://localhost:3000/api/rest");
-  // const response = await res.json();
-  // console.log("res new", response);
-  // const data = await fetchData();
-  // console.log(data);
+  const data = await fetchData();
+  console.log(data);
   return (
     <>
       <h1>nasir</h1>
-      {/* {data.map((item: any) => {
+      {data.map((item: any) => {
         return (
           <div key={item._id}>
             <div>{item.name}</div>
           </div>
         );
-      })} */}
+      })}
     </>
   );
 }
---
+++ src/model/userModel.ts
@@ -0,0 +1,8 @@
+import mongoose from "mongoose";
+
+const userModel = new mongoose.Schema({
+  name: String,
+});
+
+export const userSchema =
+  mongoose.models.users || mongoose.model("users", userModel);
