d8cbc8aa8944409175e7bcee757c51955772b9b7
nasirabbaskhan <akhtarabbas.islamicreaction@gmail.com>
2024-04-06 00:40:29 -0700

    godd to go

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,8 +5,8 @@
 
 // Your fetchData function implementation
 const fetchData = async () => {
-  const res = await fetch("https://restaurant-ya6d.vercel.app/api/rest", {
-    cache: "no-store",
+  const res = await fetch("http://localhost:3000/api/rest", {
+    cache: "no-cache",
   });
   const response = await res.json();
   console.log("res new", response);
